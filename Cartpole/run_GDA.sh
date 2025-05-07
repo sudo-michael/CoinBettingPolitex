8dd8d9f2f81b52cec1cd2eadb58ada6c2ad6dcf8
mla233 <mla233@cs-star.cmpt.sfu.ca>
2025-05-07 16:46:19 -0700

    modify slurm scripts

--- Cartpole/run_GDA.sh
+++ Cartpole/run_GDA.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-#SBATCH --mem=800M	                      # Ask for 2 GB of RAM
-#SBATCH --gres=gpu:1             # Number of GPUs (per node)
-#SBATCH --time=17:00:00                   # The job will run for 3 hours
+#SBATCH -J gda
+#SBATCH --mem=4G	         # Ask for 2 GB of RAM
+#SBATCH --gres=gpu:0             # Number of GPUs (per node)
+#SBATCH --time=8:00
 #SBATCH --output=./out/%j-%x.out
 #SBATCH --error=./err/%j-%x.err
-module load python/3.7
-source ~/.bashrc
-source ~/CMDPenv/bin/activate
+#SBATCH --partition=mars-lab-short
+#SBATCH --nodelist=cs-venus-13
+
 iters=$1
 run=$2
 lrp=$3
@@ -16,4 +17,4 @@ entropy=$6
 cmdp=$7
 path_name="./"
 file_name="trainCartpole_GDA.py"
-python $path_name$file_name --num_iterations $iters --run $run --learning_rate_pol $lrp --learning_rate_lambd $lrl --num_samples $samples --entropy_coeff $entropy --cmdp $cmdp
+uv run python $path_name$file_name --num_iterations $iters --run $run --learning_rate_pol $lrp --learning_rate_lambd $lrl --num_samples $samples --entropy_coeff $entropy --cmdp $cmdp
